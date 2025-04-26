8a55f541d7f02d7f5a5f5d5df2a711500d0ae156
Benelzane-Kholani <benzemabenelzane@yahoo.com>
2025-04-26 10:58:29 +0200

    add progess bar

---
+++ src/app/components/progress.tsx
@@ -0,0 +1,8 @@
+
+export default function Progress(){
+    return(
+        <div className=" text-neutral-800 bg-zinc-100 border-1">
+            <div className="site-progress text-center">React site still in progress.</div>
+        </div>
+    )
+}
